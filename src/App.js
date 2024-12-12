8d3ba121fa6bad5b13b271c380005ef914fe1ab0
swapnil sahu <swapnil28.sahu@gmail.com>
2024-12-13 01:23:03 +0530

    exploring different methods

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 const express= require("express");
 const app =express();
+app.get("/user",(req,res)=>{
+    res.send({name:" swapnil",lastname:"sahu"})
+});
+app.post("/user",(req,res)=>{
+    res.send("post call has been made for user updates")
+});
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully")
+});
+app.use("/",(req,res)=>{
+    res.send("hello from server 888")
+});// request handler
 app.use("/inside",(req,res)=>{
     res.send("hello from server inside")
 });
-app.use("/test",(req,res)=>{
-    res.send("testing is writen in server inside")
-});
+
 app.use("/hi",(req,res)=>{
     res.send("hello and welcome")
 });
