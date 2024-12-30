821c1763567cbb3547c9e01acc07ae75ee5d187d
swapnil sahu <swapnil28.sahu@gmail.com>
2024-12-30 10:01:02 +0530

    Error handling

--- src/App.js
+++ src/App.js
@@ -36,22 +36,38 @@ const {adminAuth, userAuth}=require("./Middleware/Auth")
 //     console.log("next one")
 // }
 // );
-app.use("/admin",adminAuth)
+// app.use("/admin",adminAuth)
 
-app.use("/user",userAuth,(req,res,next)=>{
-res.send("hello and welcome from dummy codes") 
-next();
+// app.use("/user",userAuth,(req,res,next)=>{
+// res.send("hello and welcome from dummy codes") 
+// next();
+// })
+
+// app.use("/admin/getData",(req,res)=>
+//     {
+//    res.send("All data fetched") 
+//     })
+// app.use("/admin/DeleteData",(req,res)=>
+// {
+//    res.send("Deleted data") 
+//  })
+
+//app.use("/route",[rh1,rh2,rh3..])
+//handling errors
+app.get("/getAlldata",(req,res)=>{
+   
+    try {
+        throw new Error("jdsjdskjdll")
+        res.send("user data sent")
+    } catch (error) {
+        res.status(501).send("error error!")
+    }
 })
-
-app.use("/admin/getData",(req,rest)=>
-    {
-   res.send("All data fetched") 
-    })
-app.use("/admin/DeleteData",(req,res)=>
-{
-   res.send("Deleted data") 
- })
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(501).send("something went wrong!!")
+    }
+})
 app.listen(888,()=>{
     console.log("server is listening to port 888")
 })
-//app.use("/route",[rh1,rh2,rh3..])
